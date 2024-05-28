306e31e32fa146ecf2617ad0ac80a5fd0085b72e
mogaltufel <168077441+mogaltufel@users.noreply.github.com>
2024-05-28 17:16:15 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {reactDescriptions[genRandomInt(1)]} React concepts you will need for
+          almost any app you are going to build!
+        </p>
+      </header>
+
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
